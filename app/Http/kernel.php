e7f745990013237c17677a43d9d42e7605a14fe0
Rosiane C S dos Passos <rosianecristinasi2013@gmail.com>
2025-11-10 12:11:47 -0300

    Alteração do kernel.php
    Correção do erro  Illuminate\Contracts\Container\BindingResolutionException: Target class [check.subscription] does not exist

--- app/Http/kernel.php
+++ app/Http/kernel.php
@@ -65,7 +65,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 
-        // SEU MIDDLEWARE PERSONALIZADO PARA VERIFICAR A ASSINATURA
-        'check.subscription' => \App\Http\Middleware\CheckSubscription::class,
+        // MIDDLEWARE PERSONALIZADO PARA VERIFICAR A ASSINATURA
+      'check.subscription' => \App\Http\Middleware\CheckSubscription::class,
     ];
 }
