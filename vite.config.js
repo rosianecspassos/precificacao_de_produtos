2f166b9e9ea9ec3af147438aaeb29bb4303b0ec3
Rosiane C S dos Passos <rosianecristinasi2013@gmail.com>
2025-11-18 15:31:06 -0300

    CSS, SCSS e Js
    Alterações para melhor layout da dashboard.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+ 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+        laravel([
+           'resources/css/app.scss',
+            'resources/js/app.js',
+              'resources/js/style.js',
+            'resources/css/app.css',
+        ]),
     ],
 });
+//npm run dev
